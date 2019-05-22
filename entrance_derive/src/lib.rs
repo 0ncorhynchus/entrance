97b5aa3582d2c0f25a9d5a2097b60f38fa78bdcc
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-05-23 01:09:19 +0900

    :tada: Initial commit

---
+++ entrance_derive/src/lib.rs
@@ -0,0 +1,41 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(Args)]
+pub fn args_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_args(&ast)
+}
+
+fn impl_args(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let fields = match &ast.data {
+        syn::Data::Struct(data) => &data.fields,
+        _ => panic!("Not supported for any type except Struct"),
+    };
+
+    let body = match fields {
+        syn::Fields::Named(fields) => {
+            let named = fields.named.iter().map(|f| f.ident.as_ref().unwrap());
+            quote! {
+                Ok(Self {
+                    #( #named: args.next().unwrap().parse()?, )*
+                })
+            }
+        }
+        _ => panic!("Not supported for any Struct without named fields"),
+    };
+
+    let gen = quote! {
+        impl Args for #name {
+            fn parse_from<I: Iterator<Item = String>>(mut args: I) -> Result<Self> {
+                args.next(); // Drop the first element;
+                #body
+            }
+        }
+    };
+    gen.into()
+}
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+pub use entrance_derive::Args;
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+pub trait Args: Sized {
+    fn parse_from<I: Iterator<Item = String>>(args: I) -> Result<Self>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn parse_from() -> Result<()> {
+        struct Arguments {
+            arg1: String,
+            arg2: i32,
+            arg3: PathBuf,
+        }
+
+        impl Args for Arguments {
+            fn parse_from<I: Iterator<Item = String>>(mut args: I) -> Result<Self> {
+                args.next();
+                Ok(Self {
+                    arg1: args.next().unwrap().parse()?,
+                    arg2: args.next().unwrap().parse()?,
+                    arg3: args.next().unwrap().parse()?,
+                })
+            }
+        }
+
+        let args = ["sample", "arg1", "123", "path/to/file"];
+        let result = Arguments::parse_from(args.iter().map(|s| s.to_string()))?;
+
+        assert_eq!(result.arg1, "arg1".to_string());
+        assert_eq!(result.arg2, 123);
+        assert_eq!(result.arg3, "path/to/file".parse::<PathBuf>().unwrap());
+
+        Ok(())
+    }
+
+    #[test]
+    fn derive_macro() -> Result<()> {
+        #[derive(Args)]
+        struct Arguments {
+            arg1: String,
+            arg2: i32,
+            arg3: PathBuf,
+        }
+
+        let args = ["sample", "arg1", "123", "path/to/file"];
+        let result = Arguments::parse_from(args.iter().map(|s| s.to_string()))?;
+
+        assert_eq!(result.arg1, "arg1".to_string());
+        assert_eq!(result.arg2, 123);
+        assert_eq!(result.arg3, "path/to/file".parse::<PathBuf>().unwrap());
+
+        Ok(())
+    }
+}
