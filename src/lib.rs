c6a88bee39c32acd00d44f050eb9c531df7f7185
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-06-02 01:20:01 +0900

    :memo: Add a root document of crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,30 @@
+//! Utilities for parsing command line arguments
+//!
+//! `entrance` provides the type assisted tools to parse command line argumuments.
+//!
+//! # Usage
+//!
+//! An example of `Command` with `Options` are:
+//!
+//! ```
+//! use entrance::*;
+//!
+//! #[derive(Options)]
+//! struct Opts {
+//!     help: bool,
+//!     verbose: bool,
+//!     version: bool,
+//! }
+//!
+//! let args = ["program", "--version"].iter().map(|s| s.to_string());
+//! let command: Command<Opts, ()> =
+//!     Command::new("program").parse_args(args).unwrap();
+//!
+//! assert!(command.options().version);
+//! assert!(!command.options().help);
+//! assert!(!command.options().verbose);
+//! ```
+
 mod arguments;
 mod command;
 mod options;
