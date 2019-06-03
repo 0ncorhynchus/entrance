617fd372b299a1537949044afb2f49c58b336c8e
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-06-03 20:04:01 +0900

    :sparkles: Add an attribute "short" to Options
    Add a new attribute "short" for members of derive(Options)

--- entrance_derive/src/lib.rs
+++ entrance_derive/src/lib.rs
@@ -11,7 +11,7 @@ pub fn args_derive(input: TokenStream) -> TokenStream {
     arguments::impl_arguments(&ast)
 }
 
-#[proc_macro_derive(Options, attributes(description))]
+#[proc_macro_derive(Options, attributes(description, short))]
 pub fn options_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     options::impl_options(&ast)
--- entrance_derive/src/options.rs
+++ entrance_derive/src/options.rs
@@ -1,4 +1,4 @@
-use crate::{extract_name_values, get_description};
+use crate::{extract_name_values, get_description, get_single_attribute};
 use proc_macro::TokenStream;
 use quote::quote;
 
@@ -19,6 +19,25 @@ fn long_option_arm(option: &syn::Ident) -> impl quote::ToTokens {
     quote!(
         stringify!(#option) => #option = true,
     )
+}
+
+fn get_short_attribute(name_values: &[syn::MetaNameValue]) -> Option<char> {
+    if let syn::Lit::Char(c) = get_single_attribute("short", name_values)? {
+        Some(c.value())
+    } else {
+        panic!("Invalid usage of `short` attribute: expected a char");
+    }
+}
+
+fn option_to_tokens<T: quote::ToTokens>(x: Option<T>) -> impl quote::ToTokens {
+    match x {
+        Some(c) => quote! {
+            Some(#c)
+        },
+        None => quote! {
+            None
+        },
+    }
 }
 
 fn impl_for_named_fields(name: &syn::Ident, fields: &syn::FieldsNamed) -> TokenStream {
@@ -80,13 +99,16 @@ fn impl_for_named_fields(name: &syn::Ident, fields: &syn::FieldsNamed) -> TokenS
         .map(|f| extract_name_values(&f.attrs))
         .collect();
     let descriptions = name_values.iter().map(|x| get_description(x));
+    let shorts = name_values
+        .iter()
+        .map(|x| option_to_tokens(get_short_attribute(x)));
     let opts_impl = quote! {
         fn spec() -> &'static [entrance::Opt] {
             static OPTS: [entrance::Opt; #num_options] = [
                 #(
                     entrance::Opt {
                         long: stringify!(#options),
-                        short: None,
+                        short: #shorts,
                         description: #descriptions,
                     },
                 )*
