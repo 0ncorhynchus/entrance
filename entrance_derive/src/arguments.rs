4f089d329afdeedf020edb7d887b625e15c803f5
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-06-30 01:31:37 +0900

    :sparkles: Add a new attribute for variable argument
    Add "variable_argument" attribute for the structs implement Arguments trait via the custom derive macro

--- entrance_derive/src/arguments.rs
+++ entrance_derive/src/arguments.rs
@@ -1,4 +1,4 @@
-use crate::{extract_name_values, get_description};
+use crate::{extract_name_values, extract_words, get_description, has_attribute};
 use proc_macro::TokenStream;
 use quote::quote;
 use std::convert::{TryFrom, TryInto};
@@ -26,6 +26,7 @@ pub fn impl_arguments(ast: &syn::DeriveInput) -> TokenStream {
 struct ArgumentItem<'a> {
     name: &'a syn::Ident,
     description: String,
+    is_variable: bool,
 }
 
 impl<'a> TryFrom<&'a syn::Field> for ArgumentItem<'a> {
@@ -38,17 +39,40 @@ impl<'a> TryFrom<&'a syn::Field> for ArgumentItem<'a> {
             .ok_or("The tuple structure is not available.")?;
         let name_value_attrs = extract_name_values(&field.attrs);
         let description = get_description(&name_value_attrs);
+        let is_variable = has_attribute("variable_argument", &extract_words(&field.attrs));
 
-        Ok(Self { name, description })
+        Ok(Self { name, description, is_variable })
     }
 }
 
-fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
-    let arguments: Vec<ArgumentItem> = fields
+fn get_arguments(fields: &syn::FieldsNamed) -> (Vec<ArgumentItem>, Option<ArgumentItem>) {
+    let mut arguments: Vec<ArgumentItem> = fields
         .named
         .iter()
         .map(|field| field.try_into().unwrap())
         .collect();
+
+    let var_arg = if let Some(arg) = arguments.last() {
+        if arg.is_variable {
+            arguments.pop()
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    for arg in &arguments {
+        if arg.is_variable {
+            panic!("The \"variable_argument\" attribute is allowed only for the last field");
+        }
+    }
+
+    (arguments, var_arg)
+}
+
+fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
+    let (arguments, var_arg) = get_arguments(fields);
 
     let parse_arms = arguments.iter().map(|argument| {
         let name = &argument.name;
@@ -61,6 +85,12 @@ fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
                 )?,
         }
     });
+    let parse_var_arg = var_arg.as_ref().map(|argument| {
+        let name = &argument.name;
+        quote! {
+            #name: entrance::parse_variable_argument(args)?,
+        }
+    });
     let parse_impl = quote! {
         fn parse<I: std::iter::Iterator<Item = std::string::String>>(
             args: &mut I
@@ -69,6 +99,7 @@ fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
                 #(
                     #parse_arms
                 )*
+                #parse_var_arg
             })
         }
     };
@@ -90,9 +121,22 @@ fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
         }
     };
 
-    let var_spec_impl = quote! {
-        fn var_spec() -> std::option::Option<entrance::Arg> {
-            None
+    let var_spec_impl = if let Some(argument) = var_arg {
+        let name = &argument.name;
+        let description = &argument.description;
+        quote! {
+            fn var_spec() -> std::option::Option<entrance::Arg> {
+                Some(entrance::Arg {
+                    name: stringify!(#name),
+                    description: #description,
+                })
+            }
+        }
+    } else {
+        quote! {
+            fn var_spec() -> std::option::Option<entrance::Arg> {
+                None
+            }
         }
     };
 
--- entrance_derive/src/lib.rs
+++ entrance_derive/src/lib.rs
@@ -5,7 +5,7 @@ mod options;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Arguments, attributes(description))]
+#[proc_macro_derive(Arguments, attributes(description, variable_argument))]
 pub fn args_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     arguments::impl_arguments(&ast)
