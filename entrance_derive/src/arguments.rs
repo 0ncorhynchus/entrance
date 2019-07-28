e1c8c5fb6c16bf31c0065f7cbbe88c946bd9d7b8
Suguru Kato <salmon.bioinformatics@gmail.com>
2019-07-28 21:10:05 +0900

    :recycle: Use syn::Parse for derive macro of Argument

--- entrance_derive/src/arguments.rs
+++ entrance_derive/src/arguments.rs
@@ -1,154 +1,163 @@
-use crate::{extract_name_values, extract_words, get_description, has_attribute};
+use crate::{extract_name_values, get_description};
 use proc_macro::TokenStream;
 use quote::quote;
-use std::convert::{TryFrom, TryInto};
+use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::{braced, Token};
 
-pub fn impl_arguments(ast: &syn::DeriveInput) -> TokenStream {
-    let name = &ast.ident;
-    let fields = match &ast.data {
-        syn::Data::Struct(data) => &data.fields,
-        _ => panic!("Not supported for any type except Struct"),
-    };
-
-    let body = match fields {
-        syn::Fields::Named(fields) => impl_for_named_fields(fields),
-        _ => panic!("Not supported for any Struct without named fields"),
-    };
-
-    let gen = quote! {
-        impl entrance::Arguments for #name {
-            #body
-        }
-    };
-    gen.into()
+pub struct ArgumentsInput {
+    _struct_token: Token![struct],
+    ident: syn::Ident,
+    _brace_token: syn::token::Brace,
+    fields: ArgumentFields,
 }
 
-struct ArgumentItem<'a> {
-    name: &'a syn::Ident,
-    description: String,
-    is_variable: bool,
-}
+impl ArgumentsInput {
+    pub fn gen(&self) -> TokenStream {
+        let parse_arms = self.fields.arguments.iter().map(|argument| {
+            let ident = &argument.ident;
+            quote! {
+                #ident:
+                    entrance::parse_argument(
+                        args
+                            .next()
+                            .ok_or(entrance::ErrorKind::InvalidNumberOfArguments)?
+                    )?,
+            }
+        });
 
-impl<'a> TryFrom<&'a syn::Field> for ArgumentItem<'a> {
-    type Error = &'static str;
+        let parse_var_arg = self.fields.variable_argument.as_ref().map(|argument| {
+            let ident = &argument.ident;
+            quote! {
+                #ident: entrance::parse_variable_argument(args)?,
+            }
+        });
 
-    fn try_from(field: &'a syn::Field) -> Result<Self, Self::Error> {
-        let name = field
-            .ident
-            .as_ref()
-            .ok_or("The tuple structure is not available.")?;
-        let name_value_attrs = extract_name_values(&field.attrs);
-        let description = get_description(&name_value_attrs);
-        let is_variable = has_attribute("variable_argument", &extract_words(&field.attrs));
+        let names = self.fields.arguments.iter().map(|argument| &argument.ident);
+        let descriptions = self
+            .fields
+            .arguments
+            .iter()
+            .map(|argument| &argument.description);
 
-        Ok(Self {
-            name,
-            description,
-            is_variable,
-        })
-    }
-}
-
-fn get_arguments(fields: &syn::FieldsNamed) -> (Vec<ArgumentItem>, Option<ArgumentItem>) {
-    let mut arguments: Vec<ArgumentItem> = fields
-        .named
-        .iter()
-        .map(|field| field.try_into().unwrap())
-        .collect();
-
-    let var_arg = if let Some(arg) = arguments.last() {
-        if arg.is_variable {
-            arguments.pop()
-        } else {
-            None
-        }
-    } else {
-        None
-    };
-
-    for arg in &arguments {
-        if arg.is_variable {
-            panic!("The \"variable_argument\" attribute is allowed only for the last field");
-        }
-    }
-
-    (arguments, var_arg)
-}
-
-fn impl_for_named_fields(fields: &syn::FieldsNamed) -> impl quote::ToTokens {
-    let (arguments, var_arg) = get_arguments(fields);
-
-    let parse_arms = arguments.iter().map(|argument| {
-        let name = &argument.name;
-        quote! {
-            #name:
-                entrance::parse_argument(
-                    args
-                        .next()
-                        .ok_or(entrance::ErrorKind::InvalidNumberOfArguments)?
-                )?,
-        }
-    });
-    let parse_var_arg = var_arg.as_ref().map(|argument| {
-        let name = &argument.name;
-        quote! {
-            #name: entrance::parse_variable_argument(args)?,
-        }
-    });
-    let parse_impl = quote! {
-        fn parse<I: std::iter::Iterator<Item = std::string::String>>(
-            args: &mut I
-        ) -> entrance::Result<Self> {
-            Ok(Self {
-                #(
-                    #parse_arms
-                )*
-                #parse_var_arg
-            })
-        }
-    };
-
-    let num_variables = arguments.len();
-    let names = arguments.iter().map(|argument| &argument.name);
-    let descriptions = arguments.iter().map(|argument| &argument.description);
-    let spec_impl = quote! {
-        fn spec() -> &'static [entrance::Arg] {
-            const ARGS: [entrance::Arg; #num_variables] = [
-                #(
-                    entrance::Arg {
-                        name: stringify!(#names),
-                        description: #descriptions,
-                    },
-                )*
-            ];
-            &ARGS
-        }
-    };
-
-    let var_spec_impl = if let Some(argument) = var_arg {
-        let name = &argument.name;
-        let description = &argument.description;
-        quote! {
-            fn var_spec() -> std::option::Option<entrance::Arg> {
+        let var_spec_impl = if let Some(argument) = &self.fields.variable_argument {
+            let ident = &argument.ident;
+            let description = &argument.description;
+            quote! {
                 Some(entrance::Arg {
-                    name: stringify!(#name),
+                    name: stringify!(#ident),
                     description: #description,
                 })
             }
-        }
-    } else {
-        quote! {
-            fn var_spec() -> std::option::Option<entrance::Arg> {
-                None
+        } else {
+            quote! { None }
+        };
+
+        let ident = &self.ident;
+        let num_arguments = self.fields.arguments.len();
+        (quote! {
+            impl entrance::Arguments for #ident {
+                fn parse<I: std::iter::Iterator<Item = std::string::String>>(
+                    args: &mut I
+                ) -> entrance::Result<Self> {
+                    Ok(Self {
+                        #(
+                            #parse_arms
+                        )*
+                        #parse_var_arg
+                    })
+                }
+
+                fn spec() -> &'static [entrance::Arg] {
+                    const ARGS: [entrance::Arg; #num_arguments] = [
+                        #(
+                            entrance::Arg{
+                                name: stringify!(#names),
+                                description: #descriptions,
+                            },
+                        )*
+                    ];
+                    &ARGS
+                }
+
+                fn var_spec() -> std::option::Option<entrance::Arg> {
+                    #var_spec_impl
+                }
             }
+        })
+        .into()
+    }
+}
+
+impl Parse for ArgumentsInput {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(Token![struct]) {
+            let content;
+            Ok(ArgumentsInput {
+                _struct_token: input.parse()?,
+                ident: input.parse()?,
+                _brace_token: braced!(content in input),
+                fields: content.parse()?,
+            })
+        } else {
+            Err(lookahead.error())
         }
-    };
+    }
+}
 
-    quote! {
-        #parse_impl
+struct Field {
+    ident: syn::Ident,
+    description: String,
+}
 
-        #spec_impl
+struct ArgumentFields {
+    arguments: Vec<Field>,
+    variable_argument: Option<Field>,
+}
 
-        #var_spec_impl
+impl Parse for ArgumentFields {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let fields: Punctuated<_, Token![,]> = input.parse_terminated(syn::Field::parse_named)?;
+        let mut arguments = Vec::new();
+        let mut prev_variable: Option<syn::Attribute> = None;
+        for field in fields {
+            if let Some(attr) = prev_variable {
+                return Err(syn::Error::new_spanned(
+                    attr,
+                    "The \"variable_argument\" attribute is allowed only for the last field",
+                ));
+            }
+
+            prev_variable = field
+                .attrs
+                .iter()
+                .filter(|attr| {
+                    if let Some(meta) = attr.interpret_meta() {
+                        if let syn::Meta::Word(ident) = meta {
+                            return ident == "variable_argument";
+                        }
+                    }
+                    false
+                })
+                .next()
+                .cloned();
+
+            arguments.push(Field {
+                ident: field.ident.unwrap(),
+                description: get_description(&extract_name_values(&field.attrs)),
+            });
+        }
+
+        let variable_argument = if prev_variable.is_some() {
+            arguments.pop()
+        } else {
+            None
+        };
+
+        Ok(Self {
+            arguments,
+            variable_argument,
+        })
     }
 }
