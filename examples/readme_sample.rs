e2ff394e417cc4253e31aea13439be3e281d46ab
Suguru Kato <salmon.bioinformatics@gmail.com>
2020-11-06 16:01:32 +0900

    :memo: Update README.md

--- examples/readme_sample.rs
+++ examples/readme_sample.rs
@@ -1,18 +1,3 @@
-entrance
-========
-
-[![Crates.io](https://img.shields.io/crates/v/entrance.svg)](https://crates.io/crates/entrance)
-[![Document](https://docs.rs/entrance/badge.svg)](https://docs.rs/entrance)
-[![CircleCI](https://circleci.com/gh/0ncorhynchus/entrance.svg?style=shield)](https://circleci.com/gh/0ncorhynchus/entrance)
-
-Type sytem assisted command line argument parser
-
-`entrance` provides type assisted tools for parsing command line arguments.
-
-Simple usage
-------------
-
-```rust
 use entrance::{Arguments, Options};
 use std::env;
 use std::path::PathBuf;
@@ -55,26 +40,3 @@ fn main() {
     println!("1st argument: \"{}\"", args.num);
     println!("2nd argument: \"{}\"", args.file.display());
 }
-```
-
-Structs and traits
-------------------
-
-### Command
-
-This struct provides tools for parsing command line arguments.
-
-Before parsing command line arguments, it is necessary to create the instance
-with the associated function `new` then, call `parse` of the instance.
-
-### Options
-
-A derive macro is available for this.
-
-Limitation: the derive macro supports only an Enum whose variants don't have any field.
-
-### Arguments
-
-A derive macro is available for this.
-
-Limitation: the macro supports only the struct with members implementing `FromStr`.
